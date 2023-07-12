c639c36639348c74daeaa32daedd12d4ec749207
Guymaster <guymaster25@gmail.com>
2023-07-12 15:53:48 +0000

    Making it simple and adding readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # firebaseauth_tokengen
+Simple auth token generator using Firebase email/password Authentication
+
+## Before starting
+
+Set up configuration data in a `config.json` file at root directory.
+```
+export const firebaseConfig = {
+    apiKey: "...",
+    authDomain: "...",
+    projectId: "...",  
+    storageBucket: "...",  
+    messagingSenderId: "...",  
+    appId: "...",  
+    measurementId: "..."
+  };
+
+export const credentialsConfig = {
+  email: "...",
+  password: "..."
+};
+```
+
+## How to use
+Press `Enter` to load a new token and click on the generated token to copy it to clipboard.
--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js';
 import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js';
-import { firebaseConfig } from './config.js';
+import { firebaseConfig, credentialsConfig } from './config.js';
 
 const app = initializeApp(firebaseConfig);
 
 function copyToClipboard(text) {
   navigator.clipboard.writeText(text);
   }
+
+function getCurrentTime(){
+    return (new Date()).toUTCString();
+}
 
 function signInWithEmail(email, password) {
     const auth = getAuth();
@@ -16,24 +20,27 @@ function signInWithEmail(email, password) {
         user.getIdToken()
         .then((token) => {
             document.getElementById("tokenBox").innerText = token;
+            document.getElementById("info").innerText = getCurrentTime();
         })
         .catch((error) => {
-            document.getElementById("tokenBox").innerText = "Please authentificate";
+            document.getElementById("tokenBox").innerText = "Wrong credentials";
             console.error( error);
         });
     })
     .catch((error) => {
-        document.getElementById("tokenBox").innerText = "Please authentificate";
+        document.getElementById("tokenBox").innerText = "An error occured";
         console.error( error);
     });
 }
 
 document.addEventListener("keydown", (event) => {
     if(event.key === "Enter"){
-        signInWithEmail(document.getElementById("email").value, document.getElementById("password").value);
+        signInWithEmail(credentialsConfig.email, credentialsConfig.password);
     }
 });
 
-document.getElementById("copy").addEventListener("click", (event) => {
-    copyToClipboard(document.getElementById("tokenBox").innerText)
+document.getElementById("tokenBox").addEventListener("click", (event) => {
+    copyToClipboard(document.getElementById("tokenBox").innerText);
 });
+
+document.getElementById("info").innerText = getCurrentTime();
